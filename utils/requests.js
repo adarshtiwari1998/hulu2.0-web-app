a8551962004fe471f838c90373e3a8594457bf97
adarshtiwari1998 <tiwari1998adarsh@gmail.com>
2021-05-15 13:41:18 +0530

    Navbar request movies title  with api from tmdb added complete

--- utils/requests.js
+++ utils/requests.js
@@ -29,5 +29,20 @@ export default {
         title: "Mystery Movies",
         url: `/discover/movie/?api_key=${API_KEY}&with_genres=9648`,
     },
-
+    fetchSciFi: {
+        title: "Sci-Fi",
+        url: `/discover/movie/?api_key=${API_KEY}&with_genres=878`,
+    },
+    fetchWestern: {
+        title: "Western Movies",
+        url: `/discover/movie/?api_key=${API_KEY}&with_genres=37`,
+    },
+    fetchAnimation: {
+        title: "Animation",
+        url: `/discover/movie/?api_key=${API_KEY}&with_genres=16`,
+    },
+    fetchTV: {
+        title: "TV Movies",
+        url: `/discover/movie/?api_key=${API_KEY}&with_genres=10770`,
+    },
 }
